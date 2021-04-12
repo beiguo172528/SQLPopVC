a63acda7d240c24d44813406bdc65489a545cc63
SQL <1042612904@qq.com>
2021-04-12 11:40:19 +0800

    first commit3

---
+++ DEMO/SQLPopViewController1/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  SQLPopViewController1
+//
+//  Created by DOFAR on 2021/4/12.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
